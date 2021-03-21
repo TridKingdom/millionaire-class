aeb2747eccdd367171c3e6feae6cb9a2eaee28b7
kavare <aarontthsieh@gmail.com>
2021-03-21 07:14:14 +0000

    [DEV] update brand color and logo

--- Gruntfile.js
+++ Gruntfile.js
@@ -201,7 +201,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
 	grunt.loadNpmTasks( 'grunt-contrib-concat' );
 	grunt.loadNpmTasks( 'grunt-contrib-watch' );
-	grunt.loadNpmTasks( 'grunt-sass' );
+	grunt.loadNpmTasks( 'grunt-contrib-sass' );
 	grunt.loadNpmTasks( 'grunt-contrib-connect' );
 	grunt.loadNpmTasks( 'grunt-autoprefixer' );
 	grunt.loadNpmTasks( 'grunt-zip' );
